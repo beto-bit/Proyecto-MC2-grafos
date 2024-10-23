3f2f7f290212f7ab3d27c130f843e7d70d38832a
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-10-22 22:15:51 -0600

    Pruebas con Tkinter

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import tkinter
-from tkinter import Tk
-  
+from tkinter import ttk, Tk
+
+
 
 def main():
     root = Tk()
     root.title("Proyecto de MC2")
+
+    frame = ttk.Frame(root, padding=40)
+    frame.grid()
+
+    ttk.Label(frame, text="Texto de Ejemplo").grid(column=0, row=0)
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=0, row=1)
 
     root.mainloop()
 
