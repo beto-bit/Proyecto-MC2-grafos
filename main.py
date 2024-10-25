7615730783716d9d64b4781061df800af094bb88
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-10-25 00:06:08 -0600

    Nombramiento

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ G = nx.Graph()
 
 
 # Funciones principales
-def AddVertex():
+def add_vertex():
     vertex_new_val = new_vertex.get().upper()
 
     # Sanity check
@@ -83,7 +83,7 @@ def AddVertex():
     create_graph_ui(G, None, 40)
 
 
-def AddEdge():
+def add_edge():
     out_vertex_val = out_vertex.get().upper()
     in_vertex_val = in_vertex.get().upper()
 
@@ -104,14 +104,14 @@ def AddEdge():
     create_graph_ui(G, None, 40)
 
 
-def ColorGradient(nodes_num: int, total_nodes: int) -> mcolors.Colormap:
+def color_gradient(nodes_num: int, total_nodes: int) -> mcolors.Colormap:
     cmap = plt.colormaps['Oranges']
     norm = mcolors.Normalize(vmin=0, vmax=total_nodes - 1)
     return cmap(norm(nodes_num))
 
 
-def BusquedaAncho(grafo, inicio):
-    queue = deque([inicio])
+def breadth_first_search(graph: nx.Graph, start: str):
+    queue = deque([start])
     visited = set()
     order = []
     while queue:
@@ -119,13 +119,13 @@ def BusquedaAncho(grafo, inicio):
         if node not in visited:
             order.append(node)
             visited.add(node)
-            neighbors = sorted(grafo.neighbors(node))
+            neighbors = sorted(graph.neighbors(node))
             queue.extend(neighbors)
     return order
 
 
-def BusquedaProfundidad(grafo, inicio):
-    stack = [inicio]
+def depth_first_search(graph: nx.Graph, start: str):
+    stack = [start]
     visited = set()
     order = []
     while stack:
@@ -133,17 +133,17 @@ def BusquedaProfundidad(grafo, inicio):
         if node not in visited:
             order.append(node)
             visited.add(node)
-            neighbors = sorted(grafo.neighbors(node), reverse=True)
+            neighbors = sorted(graph.neighbors(node), reverse=True)
             stack.extend(neighbors)
     return order
 
 
-def create_graph_ui(grafo, order=None, posY=None):
-    pos = nx.planar_layout(grafo)
+def create_graph_ui(graph: nx.Graph, order=None, posY=None):
+    pos = nx.planar_layout(graph)
     fig, ax = plt.subplots(figsize=(4, 3))
-    node_colors = [ColorGradient(i, len(grafo)) for i in range(len(grafo))]
+    node_colors = [color_gradient(i, len(graph)) for i in range(len(graph))]
     nx.draw(
-        grafo,
+        graph,
         pos,
         ax=ax,
         with_labels=True,
@@ -155,16 +155,16 @@ def create_graph_ui(grafo, order=None, posY=None):
     if order:
         for i in range(len(order)):
             nx.draw_networkx_nodes(
-                grafo,
+                graph,
                 pos,
                 nodelist=[order[i]],
                 node_color=[node_colors[i]],
                 ax=ax
             )
         for i in range(len(order)-1):
-            if (order[i], order[i + 1]) in grafo.edges or (order[i + 1], order[i]) in grafo.edges:
+            if (order[i], order[i + 1]) in graph.edges or (order[i + 1], order[i]) in graph.edges:
                 nx.draw_networkx_edges(
-                    grafo,
+                    graph,
                     pos,
                     edgelist=[(order[i], order[i + 1])],
                     width=1,
@@ -173,27 +173,26 @@ def create_graph_ui(grafo, order=None, posY=None):
                     ax=ax
                 )
 
-            #nx.draw_networkx_edges(grafo, pos, edgelist=[(order[i], order[i+1])], width=2, edge_color='r', style='dashed', ax=ax)
     canvas_fig = FigureCanvasTkAgg(fig, master=frame)
     canvas_fig.draw()
     canvas_fig.get_tk_widget().place(x=640, y=posY)
 
 
-def iniciar_busqueda_ancho():
+def start_bfs():
     if vertices:
-        order = BusquedaAncho(G, vertices[0])
+        order = breadth_first_search(G, vertices[0])
         visit_order.set(", ".join(order))
         create_graph_ui(G, order, 370)
 
 
-def iniciar_busqueda_profundidad():
+def start_dfs():
     if vertices:
-        order = BusquedaProfundidad(G, vertices[0])
+        order = depth_first_search(G, vertices[0])
         visit_order_depth.set(", ".join(order))
         create_graph_ui(G, order, 370)
 
 
-# Elementos de la interfaz
+### Elementos de la interfaz
 Entry(frame, textvariable=new_vertex, bg="#9370DB").place(x=250, y=50, width=150)
 
 ttk.Label(
@@ -205,22 +204,25 @@ ttk.Label(
 ttk.Button(
     frame,
     text="Guardar Vértice",
-    command=AddVertex,
+    command=add_vertex,
     style="Rounded.TButton"
 ).place(x=420, y=40, width=150, height=40)
 
 vertices_field = Text(frame, width=16, height=6)
 vertices_field.place(x=250, y=100)
+
 ttk.Label(frame, text="Vértices disponibles:", style="Rounded.TLabel").place(x=50, y=100)
 
 Entry(frame, textvariable=out_vertex, bg="#9370DB").place(x=250, y=220, width=150)
 ttk.Label(frame, text="Vértice de salida:", style="Rounded.TLabel").place(x=50, y=220)
+
 Entry(frame, textvariable=in_vertex, bg="#9370DB").place(x=250, y=250, width=150)
 ttk.Label(frame, text="Vértice de llegada:", style="Rounded.TLabel").place(x=50, y=250)
+
 ttk.Button(
     frame,
     text="Guardar Arista",
-    command=AddEdge,
+    command=add_edge,
     style="Rounded.TButton"
 ).place(x=420, y=225, width=150, height=40)
 
@@ -228,7 +230,7 @@ ttk.Button(
 ttk.Button(
     frame,
     text="Búsqueda a lo Ancho",
-    command=iniciar_busqueda_ancho,
+    command=start_bfs,
     style="Rounded.TButton"
 ).place(x=250, y=320, width=150, height=40)
 
@@ -237,14 +239,14 @@ ttk.Label(frame, textvariable=visit_order, style="Rounded.TLabel").place(x=420, 
 ttk.Button(
     frame,
     text="Búsqueda a lo Profundo",
-    command=iniciar_busqueda_profundidad,
+    command=start_dfs,
     style="Rounded.TButton"
 ).place(x=250, y=370, width=150, height=40)
+
 ttk.Label(frame, textvariable=visit_order_depth, style="Rounded.TLabel").place(x=420, y=370)
 
-#------------------- LÍNEAS DIVISORIAS ------------------------
+# Línea Divisoria
 ttk.Separator(frame, orient='vertical').place(x=600, y=0, relheight=80)  # Línea vertical divisoria
-# ttk.Separator(miFrame, orient='horizontal').place(x=0, y=250, relwidth=1)  # Línea horizontal divisoria
 
 
 root.mainloop()
